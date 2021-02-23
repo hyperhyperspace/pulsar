d4a4c87b89ec0c07247eaf93edfbd6ecc8d6e22e
joigno <30682875+joigno@users.noreply.github.com>
2021-02-23 23:52:32 +0100

    BigInt and floating-point calculations for stake vs difficulty.

--- src/model/stakes.ts
+++ src/model/stakes.ts
@@ -1,31 +1,44 @@
 // https://evgenus.github.io/bigint-typescript-definitions/index.html
 import { BigInt } from '@bigint-typescript-definitions';
 
-const STAKING_VDF_QUANTUM = 2000;
-const VDF_PROTECTION_BASE = 3;
-const NOISE_FRACTION = 0.10
+// Constants
+const NOISE_FRACTION = Number(0.10);
+// Variables, going to be dynamically adjusted
+const VDF_PROTECTION_BASE = Number(3.0); // will raise very slow when VDF speed gets faster
+const BLOCK_TIME_ADJUSTMENT = Number(2000);
+
+function slotByStake(coins: bigint, totalCoins: bigint, vrfSeed: bigint): bigint {
+    var stake = Number(coins) / Number(totalCoins);
+    var slot = Math.ceil(Number(1.0) / stake);
+    if (slot > 2 ** 32 - 1)
+        slot = 2 ** 32 - 1;
+    var randomSlot = BigInt.mod(vrfSeed, BigInt(slot)) + BigInt(1);
+    return randomSlot;
+}
 
 function slotByStakeWithNoise(coins: bigint, totalCoins: bigint, vrfSeed: bigint): Number {
     var stake = Number(coins) / Number(totalCoins);
-    var slot = Math.ceil(Number(1.0)/stake);
-    if (slot > 2**32 - 1)
-        slot = 2**32 - 1;
+    var slot = Math.ceil(Number(1.0) / stake);
+    if (slot > 2 ** 32 - 1)
+        slot = 2 ** 32 - 1;
     var randomSlot = BigInt.mod(vrfSeed, BigInt(slot)) + BigInt(1);
     // Noise
-    var noise = Number(BigInt.mod(vrfSeed, BigInt(2)**BigInt(256)));
-    noise /= Number(BigInt(2)**BigInt(256));
+    var noise = Number(BigInt.mod(vrfSeed, BigInt(2) ** BigInt(256)));
+    noise /= Number(BigInt(2) ** BigInt(256));
     noise -= 0.5;
     noise *= NOISE_FRACTION;
     return Number(randomSlot) + noise;
 }
 
-function slotByStake(coins: bigint, totalCoins: bigint, vrfSeed: bigint): bigint {
-    var stake = Number(coins) / Number(totalCoins);
-    var slot = Math.ceil(Number(1.0)/stake);
-    if (slot > 2**32 - 1)
-        slot = 2**32 - 1;
-    var randomSlot = BigInt.mod(vrfSeed, BigInt(slot)) + BigInt(1);
-    return randomSlot;
+function slotByStakeProtected(coins: bigint, totalCoins: bigint, vrfSeed: bigint): Number {
+    var randomSlot = slotByStakeWithNoise(coins, totalCoins, vrfSeed);
+    return VDF_PROTECTION_BASE ** Number(randomSlot);
 }
 
-    
+function vdfStepsByStakeDiscreteProtected(coins: bigint, totalCoins: bigint, vrfSeed: bigint): bigint {
+    var slotProtected = slotByStakeProtected(coins, totalCoins, vrfSeed);
+    return BigInt(Math.floor(BLOCK_TIME_ADJUSTMENT * Number(slotProtected)));
+}
+
+
+
