52bbd58fada925b7c6847fab11198ee16c871bc5
joigno <30682875+joigno@users.noreply.github.com>
2021-03-01 11:54:24 +0100

    Fixed BigInt literal.

--- test/stakes.test.ts
+++ test/stakes.test.ts
@@ -2,6 +2,6 @@ import { slotByStake } from '../src/model/stakes';
 
 describe('[RNG] Pseudo-randomness', () => {
     test('[STK01] Staking.', () => {
-        expect(slotByStake(BigInt(10),BigInt(100),BigInt(12345))).toEqual(1);
+        expect(slotByStake(BigInt(10),BigInt(100),BigInt(12345))).toEqual(BigInt(6));
     })
 });
