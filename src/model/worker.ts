c7cc1490d6ef8313a2e5f822bb500ba86e316043
Santi Bazerque <sbazerque@gmail.com>
2021-06-19 00:16:52 -0300

    moved log messages to a logger

--- src/model/worker.ts
+++ src/model/worker.ts
@@ -1,11 +1,15 @@
 import { HashedObject, LiteralContext } from '@hyper-hyper-space/core';
 import { parentPort } from 'worker_threads';
+import { Logger, LogLevel } from '../../../core/dist/util/logging';
 //import { Blockchain } from './Blockchain';
 import { BlockOp } from './BlockOp';
 import { VDF } from './VDF';
 
 
 class VDFWorker {
+
+    static logger = new Logger(VDFWorker.name, LogLevel.INFO);
+
     static start() {
     
             parentPort?.on('message', async (q: {challenge: string, steps: bigint, prevOpContext?: LiteralContext, bootstrap: boolean}) => {
@@ -25,24 +29,30 @@ class VDFWorker {
                     const bootstrapSteps = comp.getConsensusBootstrapDifficulty();
 
                     //console.log('Bootstrap VDF Steps: ' + bootstrapSteps + ' steps');
-                    console.log('Racing for bootstrap challenge (' + bootstrapSteps + ' steps): "' + q.challenge + '".');
+                    VDFWorker.logger.trace('Computing bootstrap VDF... (' + bootstrapSteps + ' steps).');
                     const tGen = Date.now();
                     challenge = await VDF.compute(q.challenge, bootstrapSteps as bigint);
                     const elapsedGen = Date.now() - tGen;
-                    console.log('Done computing Boostrap VDF, took ' + elapsedGen + ' millisecs')  ;
-                    //const tVerif = Date.now();
-                    console.log('Result Proof length (bytes) = ', challenge.length / 2)
-                    console.log('Bootstrap VDF self verification: ' + await VDF.verify(q.challenge, bootstrapSteps, challenge));
+                    VDFWorker.logger.info('Done computing Boostrap VDF, did ' + bootstrapSteps + ' steps in ' + elapsedGen + ' ms')  ;
+                    VDFWorker.logger.trace('Result Proof length (bytes) = ', challenge.length / 2)
+                    if (VDFWorker.logger.level <= LogLevel.TRACE) {
+                        VDFWorker.logger.trace('Bootstrap VDF self verification: ' + await VDF.verify(q.challenge, bootstrapSteps, challenge));
+                    }
+                    
                     //const elapsedVerif = Date.now() - tVerif;
                     //console.log('verification took ' + elapsedVerif + ' millisecs');
                 } else {
                     challenge = q.challenge;
                 }
-
-
-                console.log('computing vdf...')
+                
+                VDFWorker.logger.trace('Computing VDF... (' + q.steps + ' steps).');
+                const tGen = Date.now();
                 let result = await VDF.compute(challenge, q.steps);
-                console.log('done vdf computation.')
+                const elapsedGen = Date.now() - tGen;
+                VDFWorker.logger.info('Done computing VDF, did ' + q.steps + ' steps in ' + elapsedGen + ' ms')  ;
+                if (VDFWorker.logger.level <= LogLevel.TRACE) {
+                    VDFWorker.logger.trace('VDF self verification: ' + await VDF.verify(challenge, q.steps, result));
+                }
 
                 if (parentPort !== undefined && parentPort !== null) {
                     parentPort.postMessage(
